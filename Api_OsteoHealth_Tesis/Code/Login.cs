4ebd6d14b01db57cbbbca3e0fdef11f6e57e3960
Lautaro <lautarogozalvez@gmail.com>
2024-10-26 08:57:08 -0300

    Agregar archivos de proyecto.

---
+++ Api_OsteoHealth_Tesis/Code/Login.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Api_OsteoHealth_Tesis.Code
+{
+    public class Login
+    {
+
+        public static bool ValidateLogin(string username, string password)
+        {
+            if (username == "admin" && password == "admin")
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Api_OsteoHealth_Tesis/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Api_OsteoHealth_Tesis.Code;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_OsteoHealth_Tesis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet(Name = "Login/AccesoUsuario")]
+        public bool LogearUsuario(string username, string password)
+        {
+            return Login.ValidateLogin(username, password);
+        }
+    }
+}
